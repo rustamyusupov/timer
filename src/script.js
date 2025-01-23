bc46381b597b9b34e3973270667ade8c9b9b04de
Rustam Yusupov <i@rstm.me>
2025-01-23 19:56:56 +0200

    Implement show/hide functionality for form submission

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', event => {
-  console.log('loaded');
+document.addEventListener('DOMContentLoaded', () => {
+  const add = document.querySelector('#add');
+  const form = document.querySelector('form');
+
+  add.addEventListener('click', () => {
+    add.classList.add('hidden');
+    form.classList.remove('hidden');
+  });
+
+  form.addEventListener('submit', e => {
+    add.classList.remove('hidden');
+    form.classList.add('hidden');
+  });
 });
