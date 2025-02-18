d1ce3ada09a04b0ffdd2e6aa61e7cc823a8360c5
Rustam Yusupov <i@rstm.me>
2025-02-18 14:25:22 +0200

    Remove pwa configuration

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
-import { VitePWA } from 'vite-plugin-pwa';
-
 export default {
   base: '/interval-timer',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
-  plugins: [
-    VitePWA({
-      manifest: {
-        name: 'Interval Timer',
-        short_name: 'Interval Timer',
-        description: 'A simple interval timer',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-      },
-      registerType: 'autoUpdate',
-    }),
-  ],
   server: {
     port: 3000,
   },
