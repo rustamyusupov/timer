6af8739cc5f1929172f495321dfbee5f28a8cf59
Rustam Yusupov <i@rstm.me>
2025-02-20 13:52:59 +0200

    Configure Rollup output asset file names in Vite build settings

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 export default {
   base: '/interval-timer',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
