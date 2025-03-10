36eeaae382e6f27a43904a32746ae64e09add77b
Rustam Yusupov <i@rstm.me>
2025-03-10 16:02:27 +0200

    Reinit project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 export default {
   base: '/timer',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
