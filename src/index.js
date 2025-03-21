a98dca8d93eefa52c49f178a2a2cb3ffc1a9bd33
Rustam Yusupov <i@rstm.me>
2025-03-21 13:34:08 +0200

    Add timer functionality with state management and control actions

--- src/index.js
+++ src/index.js
@@ -1,87 +1,20 @@
 import { render } from './render';
-
-// TODO: incapsulate state and functions
-const state = {
-  intervalId: null,
-  isRunning: false,
-  seconds: 0,
-  timerIdx: 0,
-  timers: [],
-};
-
-const stopTimer = () => clearInterval(state.intervalId);
-
-const resetTimer = () => {
-  state.isRunning = false;
-  state.seconds = 0;
-  state.timerIdx = 0;
-
-  state.timers.forEach(timer => {
-    timer.active = false;
-  });
-
-  stopTimer();
-};
-
-const nextTimer = () => {
-  state.timers[state.timerIdx].active = false;
-  state.timerIdx += 1;
-
-  if (state.timerIdx === state.timers.length) {
-    resetTimer();
-    return;
-  }
-
-  state.seconds = state.timers[state.timerIdx].time;
-  state.timers[state.timerIdx].active = true;
-};
-
-const startTimer = () => {
-  state.intervalId = setInterval(() => {
-    state.seconds -= 1;
-
-    if (state.seconds === 0) {
-      nextTimer();
-    }
-
-    render(state);
-  }, 1000);
-};
-
-const handleToggle = () => {
-  state.isRunning = !state.isRunning;
-
-  if (state.isRunning) {
-    state.seconds = state.seconds || state.timers[state.timerIdx].time;
-    state.timers[state.timerIdx].active = true;
-    startTimer();
-  } else {
-    stopTimer();
-  }
-
-  render(state);
-};
-
-const handleReset = () => {
-  resetTimer();
-  render(state);
-};
+import { createTimer } from './timer';
 
 const init = () => {
-  // TODO: load timers from local storage or url
-  state.timers = [
-    { id: 0, name: 'Timer 1', time: 4 },
-    { id: 1, name: 'Timer 2', time: 2 },
-    { id: 2, name: 'Timer 3', time: 3 },
-  ];
-
+  const timer = createTimer();
   const toggle = document.getElementById('toggle');
   const reset = document.getElementById('reset');
 
-  toggle.addEventListener('click', handleToggle);
-  reset.addEventListener('click', handleReset);
+  timer.setUpdate(render);
+  timer.setTimers([
+    { id: 0, name: 'Timer 1', time: 4 },
+    { id: 1, name: 'Timer 2', time: 2 },
+    { id: 2, name: 'Timer 3', time: 3 },
+  ]);
 
-  render(state);
+  toggle.addEventListener('click', timer.toggle);
+  reset.addEventListener('click', timer.reset);
 };
 
 document.addEventListener('DOMContentLoaded', init);
---
+++ src/timer.js
@@ -0,0 +1,74 @@
+export const createTimer = () => {
+  const state = {
+    intervalId: null,
+    isRunning: false,
+    seconds: 0,
+    timerIdx: 0,
+    timers: [],
+    onUpdate: () => {},
+  };
+
+  const actions = {
+    getState: () => ({ ...state }),
+    reset: () => {
+      state.isRunning = false;
+      state.seconds = 0;
+      state.timerIdx = 0;
+
+      state.timers.forEach(timer => {
+        timer.active = false;
+      });
+
+      clearInterval(state.intervalId);
+      state.onUpdate(state);
+    },
+    next: () => {
+      state.timers[state.timerIdx].active = false;
+      state.timerIdx += 1;
+
+      if (state.timerIdx === state.timers.length) {
+        actions.reset();
+        return;
+      }
+
+      state.seconds = state.timers[state.timerIdx].time;
+      state.timers[state.timerIdx].active = true;
+      state.onUpdate(state);
+    },
+    start: () => {
+      state.intervalId = setInterval(() => {
+        state.seconds -= 1;
+        state.onUpdate(state);
+
+        if (state.seconds === 0) {
+          actions.next();
+        }
+      }, 1000);
+    },
+    stop: () => {
+      clearInterval(state.intervalId);
+      state.onUpdate(state);
+    },
+    toggle: () => {
+      state.isRunning = !state.isRunning;
+
+      if (state.isRunning) {
+        state.seconds = state.seconds || state.timers[state.timerIdx].time;
+        state.timers[state.timerIdx].active = true;
+        actions.start();
+      } else {
+        actions.stop();
+      }
+      state.onUpdate(state);
+    },
+    setTimers: timers => {
+      state.timers = timers;
+      state.onUpdate(state);
+    },
+    setUpdate: fn => {
+      state.onUpdate = fn;
+    },
+  };
+
+  return actions;
+};
