671a3e05ccae9c268bf7b9ebc228f6d94c060f46
Rustam Yusupov <i@rstm.me>
2025-03-21 18:55:53 +0200

    Implement timer persistence with local storage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
+import { getFromStorage } from './utils';
 import { createTimer } from './timer';
 import { render } from './render';
 
 const init = () => {
   const toggle = document.getElementById('toggle');
   const reset = document.getElementById('reset');
+  const timers = getFromStorage('timers') || [];
   const timer = createTimer();
 
   toggle.addEventListener('click', timer.toggle);
   reset.addEventListener('click', timer.reset);
 
   timer.setUpdate(render);
-  timer.setTimers([
-    { id: 0, name: 'Timer 1', time: 4 },
-    { id: 1, name: 'Timer 2', time: 2 },
-    { id: 2, name: 'Timer 3', time: 3 },
-  ]);
-  // TODO: load timers from local storage or url
+  timer.setTimers(timers);
 };
 
 document.addEventListener('DOMContentLoaded', init);
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,12 @@ export const formatTime = time =>
   `${Math.floor(time / 60)
     .toString()
     .padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
+
+export const getFromStorage = key => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+};
+
+export const setToStorage = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
