b068243c4f6116fead8a5d14f2723f694e9ece7d
Rustam Yusupov <i@rstm.me>
2025-03-21 19:57:31 +0200

    Add delay utility function

--- src/utils.js
+++ src/utils.js
@@ -37,3 +37,5 @@ export const formatTime = time =>
   `${Math.floor(time / 60)
     .toString()
     .padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
+
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
