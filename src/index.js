9749d2e3656b7a9527b8922d9139ae740dd7de0d
Rustam Yusupov <i@rstm.me>
2025-03-26 11:27:50 +0200

    Refactor wake lock management in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { beep, speak, enableAudio, preventSleep } from './sound';
 import { initTimers, compose, setToStorage } from './utils';
+import { beep, speak, enableAudio } from './sound';
+import { createWakeLock } from './wakelock';
 import { createTimer } from './timer';
 import { render } from './render';
 
 const init = async () => {
   const toggle = document.getElementById('toggle');
   const reset = document.getElementById('reset');
-  const sleepPreventer = preventSleep();
   const timers = await initTimers();
+  const lock = createWakeLock();
 
   const timer = createTimer({
     timers,
@@ -15,11 +16,11 @@ const init = async () => {
     onStart: speak,
     onUpdate: render,
     onRun: () => beep(0, 0),
-    onComplete: sleepPreventer.stop,
+    onComplete: lock.disable,
     onTick: s => s <= 3 && s > 0 && beep(100),
   });
 
-  const togglePreventer = () => sleepPreventer?.[timer.getState().isRunning ? 'start' : 'stop']();
+  const togglePreventer = () => lock?.[timer.getState().isRunning ? 'enable' : 'disable']();
 
   toggle.addEventListener('click', compose(timer.toggle, togglePreventer));
   reset.addEventListener('click', timer.reset);
